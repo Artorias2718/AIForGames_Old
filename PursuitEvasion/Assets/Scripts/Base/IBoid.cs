87fd00fd981303ade5f86ed8d7f5e35c811d9e8a
Artorias2718 <arturoordonez271@gmail.com>
2019-07-06 02:11:21 -0600

    Added PursuitEvasion demo.

---
+++ PursuitEvasion/Assets/Scripts/Base/IBoid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBoid 
+{
+    #region Variables
+
+    #endregion
+
+    #region Getters_Setters
+
+    float Max_Velocity{ get; }
+    float Max_Steering_Acceleration { get; }
+
+    Vector2 Position { get; set; }
+    Vector2 Velocity { get; set; }
+    Vector2 Steering { get; set; }
+
+    #endregion
+
+    #region Unity
+
+    #endregion
+
+    #region Custom
+
+    #endregion
+}
---
+++ PursuitEvasion/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(IBoid))]
+public class Player : MonoBehaviour
+{
+    #region Variables
+    private IBoid m_boid;
+    #endregion
+
+    #region Getters_Setters
+
+    #endregion
+
+    #region Unity
+    private void Awake()
+	{
+        m_boid = GetComponent<IBoid>();
+    }
+	
+	private void Update()
+	{
+        Vector2 steering = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        m_boid.Steering = steering;
+    }
+	#endregion
+
+	#region Custom
+
+	#endregion
+}
